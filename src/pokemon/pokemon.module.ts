c5db1bdb35a08929034e754a097013db46c3a285
Gildardo Alvarado <galvarado@is4tech.com>
2023-03-21 11:09:32 -0600

    Fin sección 8

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -14,6 +14,7 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
         schema: PokemonSchema,
       }
     ])
-  ]
+  ],
+  exports: [PokemonModule, MongooseModule],
 })
 export class PokemonModule { }
