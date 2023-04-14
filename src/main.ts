93aad24c4af1c1ea05bbcd1104739deaf48ba8c1
Gildardo Alvarado <glgildardo@gmail.com>
2023-04-14 09:37:32 -0600

    Add env configuration

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
     })
   )
 
-  await app.listen(3000);
+  await app.listen( process.env.PORT );
+  console.log(`App runing in port: ${ process.env.PORT }` );
 }
 bootstrap();
